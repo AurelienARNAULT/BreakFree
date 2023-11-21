7e6a7ac1cfa1888c5cae6ecb0eadf955f9866b8a
AurélienARNAULT <aurelien.arnault@etu.univ-cotedazur.fr>
2023-11-21 11:00:04 +0100

    hello world tablette

--- Tablette/BreakFreeTelephone/src/page/About.tsx
+++ Tablette/BreakFreeTelephone/src/page/About.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import SocketComponent from './SocketComponent';
 
 const About = () => {
   return (
     <View style={styles.container}>
       <Text>About Page</Text>
+      <SafeAreaView>
+        <SocketComponent />
+      </SafeAreaView>
     </View>
   );
 };
---
+++ Tablette/BreakFreeTelephone/src/page/SocketComponent.tsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button} from 'react-native';
+import io from 'socket.io-client';
+
+const SocketComponent = () => {
+  const [message, setMessage] = useState('');
+  const socket = io('http://10.212.98.199:3000'); // Création de l'instance Socket
+
+  useEffect(() => {
+    console.log('Connexion Socket établie.');
+
+    // Écouter les messages entrants
+    socket.on('onMessage', data => {
+      console.log('Message reçu:', data);
+      setMessage(data.msg + ': ' + data.content);
+    });
+
+    socket.on('connect', () => {
+      console.log('Socket connecté.');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Socket déconnecté.');
+    });
+
+    socket.on('connect_error', err => {
+      console.error('Erreur de connexion:', err);
+    });
+
+    // Fonction de nettoyage
+    return () => {
+      console.log('Déconnexion Socket.');
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  const sendMessage = () => {
+    console.log("Envoi d'un message...");
+    socket.emit('newMessage', 'Hello from Client', (response: any) => {
+      console.log('Réponse du serveur:', response);
+    });
+  };
+
+  return (
+    <View>
+      <Text>{message}</Text>
+      <Button title="Send Message" onPress={sendMessage} />
+    </View>
+  );
+};
+
+export default SocketComponent;
