12b5418adeede5fe11a461b4963b393686b44e9a
AurélienARNAULT <aurelien.arnault@etu.univ-cotedazur.fr>
2023-11-21 09:31:42 +0100

    init tablette push

---
+++ Tablette/BreakFreeTelephone/App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import About from './src/page/About'; // Chemin vers votre nouvelle page
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
