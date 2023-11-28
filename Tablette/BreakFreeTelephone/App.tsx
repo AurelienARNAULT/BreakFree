40d333878cfd16c89fd5788f625f0d4bbb8dbe7b
AurélienARNAULT <aurelien.arnault@etu.univ-cotedazur.fr>
2023-11-28 17:39:12 +0100

    main page

--- Tablette/BreakFreeTelephone/App.tsx
+++ Tablette/BreakFreeTelephone/App.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import About from './src/page/About'; // Chemin vers votre nouvelle page
+import Main from './src/page/Main';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="About" component={About} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ Tablette/BreakFreeTelephone/src/page/Main.tsx
@@ -0,0 +1,150 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from 'react-native';
+
+const App = () => {
+  const [orientation, setOrientation] = useState('portrait');
+  const [windowWidth, setWindowWidth] = useState(
+    Dimensions.get('window').width,
+  );
+  const [windowHeight, setWindowHeight] = useState(
+    Dimensions.get('window').height,
+  );
+
+  useEffect(() => {
+    const updateLayout = () => {
+      const width = Dimensions.get('window').width;
+      const height = Dimensions.get('window').height;
+      setWindowWidth(width);
+      setWindowHeight(height);
+      setOrientation(width > height ? 'landscape' : 'portrait');
+    };
+
+    const subscription = Dimensions.addEventListener('change', updateLayout);
+    updateLayout();
+
+    return () => subscription.remove();
+  }, []);
+
+  // Calcul dynamique des dimensions du bouton en fonction de l'orientation et de la taille de la fenêtre
+  const buttonWidth =
+    orientation === 'landscape' ? windowWidth * 0.2 : windowWidth * 0.3;
+  const buttonHeight =
+    orientation === 'landscape' ? windowHeight * 0.4 : windowHeight * 0.4;
+  const margindin = orientation === 'landscape' ? 20 : 40;
+
+  return (
+    <ImageBackground
+      source={require('../ressources/1599665227fond_parchemin.jpg')} // Remplacez par votre image de fond
+      style={styles.backgroundImage}
+      resizeMode="cover">
+      <View
+        style={
+          orientation === 'landscape'
+            ? styles.containerLandscape
+            : styles.container
+        }>
+        <View style={styles.grid}>
+          <TouchableOpacity
+            style={styles.gridItem}
+            onPress={() => console.log('Bouton 1 cliqué')}>
+            <Image
+              source={require('../ressources/Poche.png')}
+              style={{
+                width: buttonWidth,
+                height: buttonHeight,
+                resizeMode: 'contain',
+                margin: margindin,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.gridItem}
+            onPress={() => console.log('Bouton 2 cliqué')}>
+            <Image
+              source={require('../ressources/Enigme.png')}
+              style={{
+                width: buttonWidth,
+                height: buttonHeight,
+                resizeMode: 'contain',
+                margin: margindin,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.gridItem}
+            onPress={() => console.log('Bouton 3 cliqué')}>
+            <Image
+              source={require('../ressources/histoire_bouton.png')}
+              style={{
+                width: buttonWidth,
+                height: buttonHeight,
+                resizeMode: 'contain',
+                margin: margindin,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.gridItem}
+            onPress={() => console.log('Bouton 4 cliqué')}>
+            <Image
+              source={require('../ressources/Scanner.png')}
+              style={{
+                width: buttonWidth,
+                height: buttonHeight,
+                resizeMode: 'contain',
+                margin: margindin,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  containerLandscape: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  gridItem: {
+    width: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+  buttonImage: {
+    width: '95%',
+    height: '45%',
+    margin: 50,
+    resizeMode: 'contain',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default App;
