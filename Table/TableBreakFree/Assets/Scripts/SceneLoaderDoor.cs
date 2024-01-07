616787f13b82b4a8391b2290b6bb9f68068bfe88
MorganeLorcery <morgane.lorcery@etu.unice.fr>
2024-01-07 17:27:01 +0100

    fix door

--- Table/TableBreakFree/Assets/Scripts/SceneLoaderDoor.cs
+++ Table/TableBreakFree/Assets/Scripts/SceneLoaderDoor.cs
@@ -23,16 +23,16 @@ public class SceneLoaderDoor : MonoBehaviour
 
     void SetDoorOpen()
     {
-        float doorRotationX = PlayerPrefs.GetFloat("DoorRotationX", 0f);
-        float doorRotationY = PlayerPrefs.GetFloat("DoorRotationY", 0f);
-        float doorRotationZ = PlayerPrefs.GetFloat("DoorRotationZ", 0f);
+        float doorRotationX = PlayerPrefs.GetFloat("DoorRotationX", 2.973f);
+        float doorRotationY = PlayerPrefs.GetFloat("DoorRotationY", -117.692f);
+        float doorRotationZ = PlayerPrefs.GetFloat("DoorRotationZ", -71.494f);
 
         Vector3 savedDoorRotation = new Vector3(doorRotationX, doorRotationY, doorRotationZ);
         this.transform.rotation = Quaternion.Euler(savedDoorRotation);
 
-        float doorPositionX = PlayerPrefs.GetFloat("DoorPositionX", 0f);
-        float doorPositionY = PlayerPrefs.GetFloat("DoorPositionY", 0f);
-        float doorPositionZ = PlayerPrefs.GetFloat("DoorPositionZ", 0f);
+        float doorPositionX = PlayerPrefs.GetFloat("DoorPositionX", 1296f);
+        float doorPositionY = PlayerPrefs.GetFloat("DoorPositionY", 151f);
+        float doorPositionZ = PlayerPrefs.GetFloat("DoorPositionZ", 90f);
 
         Vector3 savedDoorPosition = new Vector3(doorPositionX, doorPositionY, doorPositionZ);
         this.transform.position = savedDoorPosition;
@@ -40,7 +40,7 @@ public class SceneLoaderDoor : MonoBehaviour
 
     void SetInitialObjectState()
     {
-        float doorRotationX = PlayerPrefs.GetFloat("DoorRotationX", 1296f);
+        float doorRotationX = PlayerPrefs.GetFloat("DoorRotationX", 2.973f);
         float doorRotationY = PlayerPrefs.GetFloat("DoorRotationY", -117.692f);
         float doorRotationZ = PlayerPrefs.GetFloat("DoorRotationZ", -71.494f);
 
