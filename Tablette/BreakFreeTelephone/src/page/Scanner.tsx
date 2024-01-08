f8f96e51706d116239a7efab9129e6685b3bfbf3
MorganeLorcery <morgane.lorcery@etu.unice.fr>
2024-01-08 17:00:24 +0100

    scan

--- Tablette/BreakFreeTelephone/src/page/Scanner.tsx
+++ Tablette/BreakFreeTelephone/src/page/Scanner.tsx
@@ -1,19 +1,27 @@
 import React, {useEffect} from 'react';
-import {View, StyleSheet} from 'react-native';
-import {RNCamera} from 'react-native-camera';
-import socket from '../socket/socket'
+import {View, StyleSheet, Text} from 'react-native'; // Ajoutez l'importation de Text
+import {RNCamera, BarCodeReadEvent} from 'react-native-camera';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import socket from '../socket/socket';
 
-const Scanner = () => {
+const Stack = createStackNavigator();
 
-    useEffect(() => {
-        socket.emit('scanned')
-    }, []);
+const HomeScreen = ({navigation}: {navigation: any}) => {
+  useEffect(() => {
+    socket.emit('scanned');
+  }, []);
+
+  const handleBarCodeRead = ({data}: BarCodeReadEvent) => {
+    // Naviguer vers une autre page avec les données du code-barres
+    navigation.navigate('BienJoue', {barcodeData: data});
+  };
 
   return (
     <View style={styles.container}>
       <RNCamera
         style={styles.preview}
-        type={RNCamera.Constants.Type.front} // Modifiez cette ligne
+        type={RNCamera.Constants.Type.front}
         flashMode={RNCamera.Constants.FlashMode.on}
         androidCameraPermissionOptions={{
           title: 'Permission d’utiliser la caméra',
@@ -22,9 +30,31 @@ const Scanner = () => {
           buttonPositive: 'Ok',
           buttonNegative: 'Annuler',
         }}
+        onBarCodeRead={handleBarCodeRead}
       />
-      {/* Votre bouton et d'autres éléments d'interface utilisateur peuvent être ici */}
     </View>
+  );
+};
+
+const BienJoueScreen = ({route}: {route: any}) => {
+  const {barcodeData} = route.params;
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Bien joué !</Text>
+      <Text>Code-barres scanné : {barcodeData}</Text>
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="BienJoue" component={BienJoueScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
@@ -41,4 +71,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Scanner;
+export default App;
